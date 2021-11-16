03eaee5e74e37a60e13f1356003dfa75adc0beb8
AndreLiu1225 <60701688+AndreLiu1225@users.noreply.github.com>
2021-11-16 21:56:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -268,7 +268,7 @@ def main():
 	app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
 	app.on_startup.append(on_startup)
 	dp.loop.set_task_factory(context.task_factory)
-    	web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))
+	web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))
 
 if __name__ == "__main__":
 	main()
